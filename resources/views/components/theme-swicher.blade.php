37a881bc725251fc24a65c66c9580aaa3fac4b4a
Ilyass Bezaiz <ibezaiz@gmail.com>
2024-05-18 20:07:01 +0100

    fixing dark mode

--- resources/views/components/theme-swicher.blade.php
+++ resources/views/components/theme-swicher.blade.php
@@ -2,25 +2,17 @@
     class="flex items-center justify-center space-x-2 mx-4">
     <input id="thisId" type="checkbox" name="switch" class="hidden" :checked="switchOn">
 
-    <svg x-show="!switchOn" @click="switchOn = true; switchTheme()" class="w-6 h-6 cursor-pointer text-gray-800"
-        xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
-        viewBox="0 0 256 256" xml:space="preserve">
-
-        <defs>
-        </defs>
-        <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;"
-            transform="translate(1.4065934065934016 1.4065934065934016) scale(2.81 2.81)">
-            <path
-                d="M 87.823 60.7 c -0.463 -0.423 -1.142 -0.506 -1.695 -0.214 c -15.834 8.398 -35.266 2.812 -44.232 -12.718 c -8.966 -15.53 -4.09 -35.149 11.101 -44.665 c 0.531 -0.332 0.796 -0.963 0.661 -1.574 c -0.134 -0.612 -0.638 -1.074 -1.259 -1.153 c -9.843 -1.265 -19.59 0.692 -28.193 5.66 C 13.8 12.041 6.356 21.743 3.246 33.35 S 1.732 57.08 7.741 67.487 c 6.008 10.407 15.709 17.851 27.316 20.961 C 38.933 89.486 42.866 90 46.774 90 c 7.795 0 15.489 -2.044 22.42 -6.046 c 8.601 -4.966 15.171 -12.43 18.997 -21.586 C 88.433 61.79 88.285 61.123 87.823 60.7 z"
-                style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(70, 70, 70); fill-rule: nonzero; opacity: 1;"
-                transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-        </g>
+    <svg wire:transition.in x-show="!switchOn" @click="switchOn = true; switchTheme()"
+        class="w-6 h-6 cursor-pointer text-gray-800" xmlns="http://www.w3.org/2000/svg"
+        style="width: 1.5em; height: 1.5em;vertical-align: middle;fill: currentColor;overflow: hidden;"
+        viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M524.8 938.666667h-4.266667a439.893333 439.893333 0 0 1-313.173333-134.4 446.293333 446.293333 0 0 1-11.093333-597.333334 432.213333 432.213333 0 0 1 170.666666-116.906666 42.666667 42.666667 0 0 1 45.226667 9.386666 42.666667 42.666667 0 0 1 10.24 42.666667 358.4 358.4 0 0 0 82.773333 375.893333 361.386667 361.386667 0 0 0 376.746667 82.773334 42.666667 42.666667 0 0 1 54.186667 55.04A433.493333 433.493333 0 0 1 836.266667 810.666667a438.613333 438.613333 0 0 1-311.466667 128z" />
     </svg>
 
-    <svg x-show="switchOn" @click="switchOn = false; switchTheme()" class="w-6 h-6 cursor-pointer"
+    <svg wire:transition.in x-show="switchOn" @click="switchOn = false; switchTheme()" class="w-6 h-6 cursor-pointer"
         xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
         viewBox="0 0 256 256" xml:space="preserve">
-
         <defs>
         </defs>
         <g style="stroke: none; stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: none; fill-rule: nonzero; opacity: 1;"
@@ -63,7 +55,5 @@
                 transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
         </g>
     </svg>
-
-
 
 </div>
