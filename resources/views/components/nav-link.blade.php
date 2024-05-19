b357754ad90b8ff21d12d4a27be5109485298abf
Ilyass Bezaiz <ibezaiz@gmail.com>
2024-05-19 10:42:13 +0100

    improve navigation menu

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,8 +3,8 @@
 @php
     $classes =
         $active ?? false
-            ? 'flex items-center py-2 px-5 text-[#5A6ACF] rounded-[30px] bg-[#707FDD] bg-opacity-10 group'
-            : 'flex items-center py-2 px-5 text-gray-900 rounded-[30px] dark:text-white hover:bg-[#707FDD] hover:bg-opacity-10 dark:hover:bg-gray-700 group';
+            ? 'flex items-center py-3 px-5 text-[#5A6ACF] rounded-[30px] bg-[#707FDD] bg-opacity-10 group'
+            : 'flex items-center py-3 px-5 text-gray-900 rounded-[30px] dark:text-white hover:bg-[#707FDD] hover:bg-opacity-10 dark:hover:bg-gray-700 group';
 @endphp
 
 <a wire:navigate {{ $attributes->merge(['class' => $classes]) }}>
